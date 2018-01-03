979b69e28a45712257ca6773bfe4711e05f78ab0
Julien Heulot <jheulot@insa-rennes.fr>
2018-01-03 12:13:22 +0100

    test

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 
-git clone https://github.com/jheulot/HEVCconformanceTest.git && \\
-./configure --disable-sdl2 --disable-sdl && make \\
-cd HEVCconformanceTest && \\
-wget https://www.dropbox.com/sh/t737d4cuqakpphi/AADLyGfMWlP5IdtTr_TqclmTa?dl=1 -O files.zip && \\
-unzip files.zip -d avconv || true && \\
-pwd && ls -al && \\
+git clone https://github.com/jheulot/HEVCconformanceTest.git
+./configure --disable-sdl2 --disable-sdl && make
+cd HEVCconformanceTest
+wget https://www.dropbox.com/sh/t737d4cuqakpphi/AADLyGfMWlP5IdtTr_TqclmTa?dl=1 -O files.zip
+unzip files.zip -d avconv || true
+pwd && ls -al
 ruby runPattern.rb -dir avconv -exec ohplay
